7cc7c4e0ad93a8513161445c994218710af24ee3
Kamirii <camilyaalbres@gmail.com>
2022-05-14 02:03:21 -0300

    first commit

---
+++ decodificacao.js
@@ -0,0 +1,23 @@
+function encodeBase64(){
+
+}
+function decodeBase64(){
+    
+}
+
+function encodeCifraCesar(){
+
+}
+function decodeCifraCesar(){
+    
+}
+function numeroTrocaPosicao(){}
+
+    var botao = 
+    document.querySelector('#botao-cifra-cesar')
+
+    var inputDeslocamento = 
+    document.querySelector('#teste')
+    botao.addEventListener('click',()=>{ inputDeslocamento.classList.toggle('show');})
+   
+
